e94983320228ec24b0168b990379601ebf4e8a37
Thomas Boerger <tboerger@suse.de>
2016-06-23 11:02:39 +0200

    Unified build script with other appc projects
    Signed-off-by: Thomas Boerger <tboerger@suse.de>

--- build.sh
+++ build.sh
@@ -8,16 +8,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ORG_PATH="github.com/appc"
 REPO_PATH="${ORG_PATH}/acpush"
 
-if [ ! -h gopath/src/${REPO_PATH} ]; then
-	mkdir -p gopath/src/${ORG_PATH}
-	ln -s ../../../.. gopath/src/${REPO_PATH} || exit 255
+if [ ! -h ${DIR}/gopath/src/${REPO_PATH} ]; then
+  mkdir -p ${DIR}/gopath/src/${ORG_PATH}
+  cd ${DIR} && ln -s ../../../.. gopath/src/${REPO_PATH} || exit 255
 fi
 
+export GO15VENDOREXPERIMENT=1
 export GOBIN=${DIR}/bin
 export GOPATH=${DIR}/gopath
+export GOOS GOARCH
 
 eval $(go env)
-export GOOS GOARCH
 
 if [ "${GOOS}" = "freebsd" ]; then
     # /usr/bin/cc is clang on freebsd, but we need to tell it to go to
@@ -26,5 +27,5 @@ if [ "${GOOS}" = "freebsd" ]; then
     export CC=clang
 fi
 
-echo "Building acpush"
-go build -o $GOBIN/acpush ${REPO_PATH}
+echo "Building acpush..."
+go build -o ${GOBIN}/acpush ${REPO_PATH}
